4cb5bb8a03486dbeedf2424153e16fe08c316be4
megha angadi <meghamangadi@gmail.com>
2021-09-03 21:11:41 +0530

    Added: Welcome Message to the User

---
+++ src/com/blz/oops/employeewage/EmployeeWage.java
@@ -0,0 +1,13 @@
+package com.blz.oops.employeewage;
+
+public class EmployeeWage {
+	public static void main(String[] args) {
+		welcome();
+
+	}
+
+	static void welcome() {
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	}
+
+}
