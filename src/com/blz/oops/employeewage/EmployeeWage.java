11eaab9c1959dc64598363f4f8a5fe4758d834d4
megha angadi <meghamangadi@gmail.com>
2021-09-03 21:22:46 +0530

    Added: Program to check Employee is Present Or Absent

--- src/com/blz/oops/employeewage/EmployeeWage.java
+++ src/com/blz/oops/employeewage/EmployeeWage.java
@@ -3,11 +3,19 @@ package com.blz.oops.employeewage;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		welcome();
-
+		attendanceCheck();
 	}
 
 	static void welcome() {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 	}
 
+	static void attendanceCheck() {
+		int isFullTime = 1;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isFullTime)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+	}
 }
