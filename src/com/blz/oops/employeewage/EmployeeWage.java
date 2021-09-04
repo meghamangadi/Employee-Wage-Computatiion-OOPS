a0cad02392e58c62afe48f5f0d096bd299ed1932
megha angadi <meghamangadi@gmail.com>
2021-09-04 07:04:59 +0530

    Modified: Program to find Part time Employee wage

--- src/com/blz/oops/employeewage/EmployeeWage.java
+++ src/com/blz/oops/employeewage/EmployeeWage.java
@@ -12,17 +12,19 @@ public class EmployeeWage {
 	}
 
 	static void emopDailyWage() {
-		int Is_Full_Time = 1;
+		int Is_Part_Time = 1;
+		int Is_Full_Time = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-		if (empCheck == Is_Full_Time)
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		if (empCheck == Is_Part_Time)
+			empHrs = 4;
+		else if (empCheck == Is_Full_Time)
 			empHrs = 8;
 		else
 			empHrs = 0;
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Employee_wage:" + empWage);
 	}
-
 }
