f7c8c13dfd51b1ae7526039f100761d0f0f4cb4d
megha angadi <meghamangadi@gmail.com>
2021-09-05 10:33:38 +0530

    Added: Program to save the total wage for each company

--- src/com/blz/oops/employeewage/EmployeeWage.java
+++ src/com/blz/oops/employeewage/EmployeeWage.java
@@ -1,16 +1,19 @@
 package com.blz.oops.employeewage;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 public class EmployeeWage {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-
+	public static Map<String, Integer> companyInfoMap = new HashMap<String,Integer>();
 	public static int computeEmpWage(String company, int empRatePerHour, int noOfWorkingDays, int maxHoursPerMonth) {
+		
 		int empHrs = 0;
 		int totalEmpHrs = 0;
-		int totalWorkingDays = 0;
-		     System.out.println("-----------------------------");
-		    System.out.println("Company Name :" + company);
-		    System.out.println("-----------------------------" );
+		int totalWorkingDays = 0;		 
 		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < noOfWorkingDays) {
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -26,20 +29,19 @@ public class EmployeeWage {
 			}
 			totalEmpHrs += empHrs;
 
-			System.out.println("Day :" + totalWorkingDays + "   Employee Hr: " + empHrs);
 		}
 
-		int totalEmpWage = totalEmpHrs * empRatePerHour;
-		 System.out.println("-----------------------------");
-		System.out.println("Total Employee Wage: " + totalEmpWage);
-		 System.out.println("-----------------------------");
+		int totalEmpWage = totalEmpHrs * empRatePerHour;			
+		companyInfoMap.put(company, totalEmpWage);	
 		return totalEmpWage;
 	}
 
 	public static void main(String[] args) {
 
-		computeEmpWage("JIO", 40, 21, 120);
+		computeEmpWage("JIO", 40, 21, 120);		
 		computeEmpWage("Wipro", 50, 24, 140);
+		System.out.println("Company Name And Total Wages:" + companyInfoMap);
+		
 	}
 
 }
