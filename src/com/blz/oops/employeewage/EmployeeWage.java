665c571b595eb651ee00486894824703e18f0d34
megha angadi <meghamangadi@gmail.com>
2021-09-05 15:12:54 +0530

    Added: Program to Manage Employee Wage For Multiple Companies

--- src/com/blz/oops/employeewage/EmployeeWage.java
+++ src/com/blz/oops/employeewage/EmployeeWage.java
@@ -1,6 +1,8 @@
 package com.blz.oops.employeewage;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -9,38 +11,55 @@ public class EmployeeWage {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static Map<String, Integer> companyInfoMap = new HashMap<String,Integer>();
-	public static int computeEmpWage(String company, int empRatePerHour, int noOfWorkingDays, int maxHoursPerMonth) {
-		
+	public static List<CompanyEmpWage> EmpWageBuilder=new ArrayList<>();
+	 public static int computeEmpWage(List<CompanyEmpWage> companies) {	
+	 
 		int empHrs = 0;
 		int totalEmpHrs = 0;
-		int totalWorkingDays = 0;		 
-		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < noOfWorkingDays) {
-			totalWorkingDays++;
-			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-			switch (empCheck) {
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
-			case IS_FULL_TIME:
-				empHrs = 8;
-				break;
-			default:
-				empHrs = 0;
+		int totalWorkingDays = 0;	
+		int totalEmpWage = 0;
+		for(int i=0;i<=companies.size()-1;i++) {
+			CompanyEmpWage comapany=companies.get(i);
+			int empRatePerHour=comapany.getEmpRatePerHr();
+			while (totalEmpHrs <= comapany.getMaxHrsPerMonth() && totalWorkingDays <comapany.getNoOfWorkingDays()) {
+				totalWorkingDays++;
+				int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+				switch (empCheck) {
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				default:
+					empHrs = 0;
+				}
+				totalEmpHrs += empHrs;
+				
 			}
-			totalEmpHrs += empHrs;
-
-		}
-
-		int totalEmpWage = totalEmpHrs * empRatePerHour;			
-		companyInfoMap.put(company, totalEmpWage);	
+			
+			 
+			 totalEmpWage = totalEmpHrs * empRatePerHour;
+			 comapany.setTotalWage(totalEmpWage);
+			 System.out.println("company name :"+comapany.getCompanyName() +"	  No of Working Days :"+comapany.getNoOfWorkingDays() +
+					 "	  Maximun hours per month :"+comapany.getMaxHrsPerMonth()+"	   Total Wages :"+comapany.getTotalWage());
+			
+		}		
+			
 		return totalEmpWage;
 	}
 
 	public static void main(String[] args) {
-
-		computeEmpWage("JIO", 40, 21, 120);		
-		computeEmpWage("Wipro", 50, 24, 140);
-		System.out.println("Company Name And Total Wages:" + companyInfoMap);
+		System.out.println("Manage  Employee wage for Multiple companies");
+		CompanyEmpWage companyJio=new CompanyEmpWage("JIO", 40, 21, 120);
+		CompanyEmpWage companyWipro=new CompanyEmpWage("Wipro", 50, 24, 140);
+		CompanyEmpWage companyTata=new CompanyEmpWage("TATA", 60, 25, 160);
+		CompanyEmpWage companyHCL=new CompanyEmpWage("HCL", 50, 22, 130);
+		EmpWageBuilder.add(companyJio);
+		EmpWageBuilder.add(companyWipro);
+		EmpWageBuilder.add(companyTata);
+		EmpWageBuilder.add(companyHCL);
+		computeEmpWage(EmpWageBuilder);
 		
 	}
 
